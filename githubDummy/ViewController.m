966386f54532c85c3dca631616a0daad61020488
Ram <Ram@Apples-MacBook-Air.local>
2017-05-24 23:46:26 +0530

    LABEL WITH HELLO MESSAGE ADDED

--- githubDummy/ViewController.m
+++ githubDummy/ViewController.m
@@ -15,6 +15,8 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
+    NSLog(@"@hello ram")
+    ;
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
 }
